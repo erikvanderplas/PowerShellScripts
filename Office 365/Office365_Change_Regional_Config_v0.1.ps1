afd9959e9b5307703fe66babcc0bd946c4ab7893
erikvanderplas <erik.van.der.plas@sogeti.nl>
2015-01-13 22:16:41 +0100

    test

---
+++ Office 365/Office365_Change_Regional_Config_v0.1.ps1
@@ -0,0 +1,14 @@
+﻿###############################################################################
+#
+#	Created by:		Erik van der Plas
+#	Company: 		Sogeti Netherlands B.V.
+#       
+# 	Created on:		January 5, 2015
+#
+#	Version:		0.1
+#
+#	Office 365 Change regional settings
+#
+###############################################################################
+
+Get-Mailbox | Set-MailboxRegionalConfiguration -LocalizeDefaultFolderName:$true
