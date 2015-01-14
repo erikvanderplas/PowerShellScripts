3933be2c940c72cb5b6173dd549ec45eb4bcf2f2
erikvanderplas <erik.van.der.plas@sogeti.nl>
2015-01-14 15:22:52 +0100

    Office365_Change_Regional_Config_v0.1.ps1

--- Office 365/Office365_Change_Regional_Config_v0.1.ps1
+++ Office 365/Office365_Change_Regional_Config_v0.1.ps1
@@ -11,4 +11,26 @@
 #
 ###############################################################################
 
-Get-Mailbox | Set-MailboxRegionalConfiguration -LocalizeDefaultFolderName:$true
+
+[CmdletBinding()]
+Param(
+	[Parameter(Mandatory=$False)]
+    [ValidateSet("English_US","English_UK","Dutch")]
+	[string]$Language
+)
+
+if (!$Language) {
+    $Language = Read-Host 'Enter a Language:
+
+     - English_US for English United States
+     - English_UK for English United Kingdom
+     - Dutch for Dutch (Netherlands)
+     '
+    }
+
+
+If ($Language -like 'English_US') { $Language = 1033 }
+If ($Language -like 'English_UK') { $Language = 2057 }
+If ($Language -like 'Dutch') { $Language = 1033 }
+
+Get-Mailbox | Set-MailboxRegionalConfiguration -Language $Language -TimeZone 'W. Europe Standard Time' | Set-MailboxRegionalConfiguration -LocalizeDefaultFolderName:$true
